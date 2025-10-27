a2d026839a04fc8963dc83fed206657bb69133b9
Yaminye <148621980+Yaminye@users.noreply.github.com>
2025-10-27 10:35:26 +0200

    Add files via upload
    react search agent

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(temperature=0, model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(llm = llm, tools=tools, prompt=react_prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
+def main():
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for ai engineer using langchain in the bay area on linkedin and list their details"
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
