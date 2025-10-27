b9529b1350c080917a7114a67be6db92d4a6d9ef
yehonatan19 <yehonatan19y@gmail.com>
2025-10-27 15:16:57 +0200

    added output parsing

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
-import os
-
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()
 
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
 tools = [TavilySearch()]
-llm = ChatOpenAI(temperature=0, model="gpt-4")
+llm = ChatOpenAI(model="gpt-5")
 react_prompt = hub.pull("hwchase17/react")
-agent = create_react_agent(llm = llm, tools=tools, prompt=react_prompt)
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
+)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
+
 
 def main():
     result = chain.invoke(
         input={
-            "input": "search for 3 job postings for ai engineer using langchain in the bay area on linkedin and list their details"
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
         }
     )
     print(result)
