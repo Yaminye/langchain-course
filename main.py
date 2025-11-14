ec8e72841db134a82875328a11caffda41e3bcc9
Yaminye <148621980+Yaminye@users.noreply.github.com>
2025-11-14 14:14:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import os
 
 from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
-
 from langchain import hub
-from langchain.agents import AgentExecutor
-from langchain.agents.react.agent import create_react_agent
-from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
-tools = [TavilySearch()]
-llm = ChatOpenAI(temperature=0, model="gpt-4")
-react_prompt = hub.pull("hwchase17/react")
-agent = create_react_agent(llm = llm, tools=tools, prompt=react_prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
-
-def main():
-    result = chain.invoke(
-        input={
-            "input": "search for 3 job postings for ai engineer using langchain in the bay area on linkedin and list their details"
-        }
-    )
-    print(result)
+load_dotenv()
 
 
 if __name__ == "__main__":
-    main()
+    print(" Retrieving...")
+
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI()
+
+    query = "what is Pinecone in machine learning?"
+    chain = PromptTemplate.from_template(template=query) | llm
+    # result = chain.invoke(input={})
+    # print(result.content)
+
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    retrival_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrival_chain.invoke(input={"input": query})
+
+    print(result)
