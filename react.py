9f68768c2a83bd849a1ff02b978c8c54bc95f72c
Yehonatan Yamin <yehonatan19y@gmail.com>
2025-11-17 19:17:08 +0200

    function calling reasoning

---
+++ react.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+@tool
+def triple(num:float) -> float:
+    """
+    patam num: a number to triple
+    return: the triple of the input number
+    """
+    return num * 3
+
+
+tool = [TavilySearch(max_results=1),triple]
+llm = ChatOpenAI(temperature=0, model="gpt-4o-mini").bind_tools(tool)
